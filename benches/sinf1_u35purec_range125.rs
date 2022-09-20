305dc9774bfc65e9e9953e46d4d5dbc965cc5c5f
Joakim Frostegård <joakim.frostegard@gmail.com>
2022-09-21 00:07:57 +0200

    Add benches/sinf1_u35purec_range125.rs

---
+++ benches/sinf1_u35purec_range125.rs
@@ -0,0 +1,19 @@
+use criterion::{criterion_group, criterion_main, BenchmarkId, Criterion};
+
+fn bench(c: &mut Criterion) {
+    let mut group = c.benchmark_group("Sleef_sinf1_u35purec_range125");
+
+    for a in [1.2, 40.8].iter() {
+        group.bench_with_input(BenchmarkId::new("Reference", a), a, |b, a| {
+            b.iter(|| unsafe { ::sleef_sys::Sleef_sinf1_u35purec(*a) })
+        });
+        group.bench_with_input(BenchmarkId::new("Ported", a), a, |b, a| {
+            b.iter(|| ::sleef_sind::Sleef_sinf1_u35purec_range125(*a))
+        });
+    }
+
+    group.finish();
+}
+
+criterion_group!(benches, bench);
+criterion_main!(benches);
