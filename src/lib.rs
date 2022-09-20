4165c076c5bbad4d09fa4938e2bf0564867f5d50
Joakim Frostegård <joakim.frostegard@gmail.com>
2022-09-21 00:01:40 +0200

    Add Sleef_sinf1_u35purec_range125 fn

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,10 @@
 #[cfg(target_arch = "x86_64")]
 mod avx;
 mod fallback;
+mod sinf1_u35purec_range125;
 mod tables;
 
 #[cfg(target_arch = "x86_64")]
 pub use avx::Sleef_sind4_u35avx;
 pub use fallback::Sleef_sind1_u35purec;
+pub use sinf1_u35purec_range125::Sleef_sinf1_u35purec_range125;
