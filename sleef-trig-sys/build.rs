ecabd7d7cdd3ab018fa5d17cf02ec92b3c4865b6
Joakim Frostegård <joakim.frostegard@gmail.com>
2022-09-26 01:21:59 +0200

    Fix sleef-trig-sys slowness by compiling header files separately

--- sleef-trig-sys/build.rs
+++ sleef-trig-sys/build.rs
@@ -1,13 +1,25 @@
 use cc::Build;
 
-const FILE: &str = "./c/x86_64.c";
-
 fn main() {
     println!("cargo:rerun-if-changed=build.rs");
     println!("cargo:rerun-if-changed=c");
 
     Build::new()
-        .file(FILE)
-        .flag("-march=native")
-        .compile("sleef_trig_sys_cc")
+        .file("./c/purec.c")
+        .warnings(false)
+        .compile("sleef_trig_purec");
+
+    #[cfg(target_arch = "x86_64")]
+    Build::new()
+        .file("./c/sse2.c")
+        .flag("-msse2")
+        .warnings(false)
+        .compile("sleef_trig_sse2");
+
+    #[cfg(target_arch = "x86_64")]
+    Build::new()
+        .file("./c/avx.c")
+        .flag("-mavx")
+        .warnings(false)
+        .compile("sleef_trig_avx");
 }
