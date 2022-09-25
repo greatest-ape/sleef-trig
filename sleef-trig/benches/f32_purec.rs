cf6396a09ff1d142ccfc67c21740d1c7febc2df4
Joakim Frostegård <joakim.frostegard@gmail.com>
2022-09-26 01:30:41 +0200

    Rename and clean up benchmarks

--- sleef-trig/benches/f32_purec.rs
+++ sleef-trig/benches/f32_purec.rs
@@ -1,6 +1,21 @@
 use criterion::{criterion_group, criterion_main, BenchmarkId, Criterion};
 
-fn bench(c: &mut Criterion) {
+fn sin(c: &mut Criterion) {
+    let mut group = c.benchmark_group("Sleef_sinf1_u35purec_range125");
+
+    for a in [1.2, 40.8].iter() {
+        group.bench_with_input(BenchmarkId::new("Reference", a), a, |b, a| {
+            b.iter(|| unsafe { ::sleef_trig_sys::Sleef_sinf1_u35purec(*a) })
+        });
+        group.bench_with_input(BenchmarkId::new("Ported", a), a, |b, a| {
+            b.iter(|| ::sleef_trig::Sleef_sinf1_u35purec_range125(*a))
+        });
+    }
+
+    group.finish();
+}
+
+fn cos(c: &mut Criterion) {
     let mut group = c.benchmark_group("Sleef_cosf1_u35purec_range125");
 
     for a in [1.2, 40.8].iter() {
@@ -15,5 +30,5 @@ fn bench(c: &mut Criterion) {
     group.finish();
 }
 
-criterion_group!(benches, bench);
+criterion_group!(benches, sin, cos);
 criterion_main!(benches);
